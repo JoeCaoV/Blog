fb5ac3fe159d5b9056d753b4b3ed8ffd40a2e6f6
Joevin Caovan <joe.caovan@gmail.com>
2019-09-26 07:24:35 +0200

    prod_settings added

---
+++ blog/blog/prod_settings.py
@@ -0,0 +1,14 @@
+import dj_database_url
+
+from .settings import *
+
+DEBUG = False
+TEMPLATE_DEBUG = False
+
+
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+
+ALLOWED_HOSTS = ['.herokuapp.com']
+
+MIDDLEWARE_CLASSES = ['whitenoise.middleware.WhiteNoiseMiddleware']
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
